16eebf3a2b44b736e5c228932c02095a3083ba55
Paul-Cavain <paulcavain01@gmail.com>
2024-10-27 09:19:29 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// const numbers = () => {
+//     const display = document.getElementById(`display`);
+//     const buttons = document.querySelectorAll(`.calculator_btn`);
+    
+//     buttons.forEach(function(button) {
+//         button.addEventListener(`click`, function(e) {
+//             let value = e.target.dataset.num;
+//             display.value += value;
+//         })
+//     })
+// }
+// numbers();
